5b62cd3dab5c749d2fff40117b99fdfb615e99f2
huzaifa iqbal <huzaifa172@users.noreply.github.com>
2025-07-28 14:19:29 +0500

    backend prisma deployement changes

--- backend/src/lib/prisma.ts
+++ backend/src/lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "../generated/prisma"; // Update path if needed
+import { PrismaClient } from '@prisma/client'; // Update path if needed
 
 const prisma = new PrismaClient();
 
