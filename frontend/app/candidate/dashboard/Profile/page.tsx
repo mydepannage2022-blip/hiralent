478624da70f66152d78a26acfe82081f4314b0eb
HirAlent <project@hiralent.com>
2025-08-12 15:16:36 +0500

    fix profile page component name

--- frontend/app/candidate/dashboard/Profile/page.tsx
+++ frontend/app/candidate/dashboard/Profile/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 
-const page = () => {
+// Change function name to ProfilePage (with capital P)
+const ProfilePage = () => {
   return (
     <div className='w-full flex justify-start items-start gap-3'>
       <div className='w-2/3 flex flex-col justify-start items-center gap-4'>
@@ -13,4 +14,4 @@ const page = () => {
   )
 }
 
-export default page
+export default ProfilePage
