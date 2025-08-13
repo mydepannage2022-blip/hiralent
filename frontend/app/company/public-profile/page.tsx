97ae1c64249c4f87c3c6b819a3b88b891a8777ed
Azlan Baig <solutionsfronx@gmail.com>
2025-08-13 18:01:59 +0500

    company public profile page under developement

--- frontend/app/company/public-profile/page.tsx
+++ frontend/app/company/public-profile/page.tsx
@@ -4,36 +4,42 @@ import React, { useState } from 'react';
 import Navbar from '../../src/components/layout/Navbar'
 import Image from 'next/image';
 import CompanyInfoCard from '../../src/components/company/public-profile/CompanyInfoCard';
+import PublicProfileTabs from '../../src/components/company/public-profile/PublicProfileTabs';
 
 const publicProfile = () => {
     return (
         <div className="text-black">
             <Navbar />
-            <div className="mt-30 mb-20">
-                {/* Banner */}
-                <div className="relative h-56 w-full overflow-hidden">
-                    <Image
-                        src="/images/publicprofile-banner.png" // your banner image path
-                        alt="Company banner"
-                        fill
-                        className="object-cover blur-sm scale-105"
-                    />
-                </div>
-                <div className="relative w-full">
-
-                    {/* Card */}
-                    <div className="relative z-10 -mt-20 px-4">
-                        <CompanyInfoCard
-                            logo="/images/bmw-logo.png"
-                            name="BMW"
-                            website="bmw.com"
-                            websiteUrl="https://bmw.com"
-                            location="Germany"
-                            companySize="1000"
-                            email="BMW@gmail.com"
-                            phone="+1 98482346"
+            <div className="mt-35 mb-20">
+                <div className='mb-12'>
+                    {/* Banner */}
+                    <div className="relative h-76 w-full overflow-hidden">
+                        <Image
+                            src="/images/publicprofile-banner.png"
+                            alt="Company banner"
+                            fill
+                            className="object-cover"
                         />
                     </div>
+
+                    <div className="relative w-full">
+                        {/* Card */}
+                        <div className="relative z-10 -mt-20 px-4">
+                            <CompanyInfoCard
+                                logo="/images/bmw-logo.png"
+                                name="BMW"
+                                website="BMW.com"
+                                websiteUrl="https://bmw.com"
+                                location="Germany"
+                                companySize="1000"
+                                email="BMW@gmail.com"
+                                phone="+1 98482346"
+                            />
+                        </div>
+                    </div>
+                </div>
+                <div className="mx-25">
+                    <PublicProfileTabs />
                 </div>
             </div>
         </div>
