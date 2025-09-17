e83835d0a753aa7913f8e950d35600da27ce01da
Azlan Baig <azlan@fronxsolutions.com>
2025-09-17 10:35:59 +0500

    just nothing

---
+++ frontend/app/footer/about/page.tsx
@@ -0,0 +1,19 @@
+import TabsNav from "../../../src/components/footer/";
+import AboutSection from "../../../src/components/footer/about/AboutSection";
+import WhoWeAre from "../../../src/components/footer/about/WhoWeAre";
+
+export default function AboutPage() {
+    return (
+        <main className="pt-10 mt-30">
+            <h1 className="text-4xl font-bold text-center mb-2">About Hiralent</h1>
+            <p className="text-center text-gray-500 mb-6">Last updated March 12, 2024</p>
+
+            {/* Tabs Navigation */}
+            <TabsNav />
+
+            {/* Sections */}
+            <AboutSection />
+            <WhoWeAre />
+        </main>
+    );
+}
